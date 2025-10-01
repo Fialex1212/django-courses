603e992419985d4ab9921bcceb7e57dac9ee2bd6
Aleks Seriakov <110390382+Fialex1212@users.noreply.github.com>
2025-10-01 16:18:27 +0300

    updated csrf

--- settings/settings.py
+++ settings/settings.py
@@ -117,8 +117,9 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://kart-podo.eu",
-    "http://localhost:3000",
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+    'http://152.53.18.199:8000',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True

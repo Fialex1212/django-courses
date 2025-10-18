c70f40be86ab438ca830f79057dfd31793133001
Aleks Seriakov <110390382+Fialex1212@users.noreply.github.com>
2025-10-18 22:43:25 +0300

    jwt fix

--- settings/settings.py
+++ settings/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "drf_spectacular_sidecar",
+    "rest_framework_simplejwt.token_blacklist",
     # apps
     "users",
     "courses",

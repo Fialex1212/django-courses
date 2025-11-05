dc4a7608211c18bad475f41b3e54b5c33b1a020f
Aleks Seriakov <110390382+Fialex1212@users.noreply.github.com>
2025-11-05 20:01:56 +0200

    fix

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet, LessonViewSet, UploadLessonVideo
+from .views import CourseViewSet, LessonViewSet
 
 router = DefaultRouter()
 router.register(r"courses", CourseViewSet)

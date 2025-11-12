6c5365b70874485dc16b1520512d1b02cf00399c
Aleks Seriakov <110390382+Fialex1212@users.noreply.github.com>
2025-11-12 18:51:26 +0200

    colored order statuses

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,4 @@
+from django.utils.html import format_html
 from django.contrib import admin
 from .models import Order
 
@@ -6,6 +7,24 @@ from .models import Order
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "course", "amount", "status", "invoice", "created_at")
     list_filter = ("status", "course")
+
+    @admin.register(Order)
+    def colored_status(self, obj):
+        if obj.status == "paid":
+            color = "green"
+        elif obj.status == "pending":
+            color = "blue"
+        elif obj.status == "canceled":
+            color = "red"
+        else:
+            color = "black"
+        return format_html(
+            '<span style="color: {};"><strong>{}</strong></span>',
+            color,
+            obj.get_status_display(),
+        )
+
+    colored_status.short_description = "Status"
     actions = ["mark_as_paid"]
 
     def mark_as_paid(self, request, queryset):
